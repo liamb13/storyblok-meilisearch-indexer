b5b66c5c90e6369fe76539d4126cfcd14c374a6d
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-07-16 13:02:52 +0200

    feat: index all content inside SB

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ class StoryblokAlgoliaIndexer {
           let records = []
           responses.forEach((response) => {
               let data = response.data
-              records = records.concat(data.stories)
+              records = records.concat(data.stories[0].content.body)
           })
-   
+
           records.forEach(record => {
-              record.objectID = record.uuid
+              record.objectID = record._uid
           })
           
           await index.saveObjects(records, { autoGenerateObjectIDIfNotExist: false }).wait().catch(e => { console.log(e) })
